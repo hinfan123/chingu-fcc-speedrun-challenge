be427fb227126840e987eab9db20bc0906422aa6
Pankajashree <pankajashree@gmail.com>
2017-03-18 13:47:42 +0530

    completed twitch.tv

--- frontend/twitch-client/scripts.js
+++ frontend/twitch-client/scripts.js
@@ -1,11 +1,9 @@
-// api call - https://wind-bow.gomix.me/twitch-api
-
 var streamers = ["FreeCodeCamp" , "ESL_SC2", "OgamingSC2", "cretetion",  "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","brunofin"];
 
 $(document).ready(function(){
-  var errUser = [];
   var i;
   for(i= 0;i<(streamers.length);i++){
+    var erruser = streamers[i];
     var url = 'https://wind-bow.gomix.me/twitch-api/channels/'+streamers[i];
 	$.ajax({
 		type: 'GET',
@@ -19,49 +17,20 @@ $(document).ready(function(){
 			$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+dataI.name).done(function(data2){
 				console.log(data2);
 				if(data2.stream === null){ //offline
-					$('#displayName').append('<li> '+myname + '</li>');
-					$("#logo").append('<li><img src = "'+mylogo+'" /></li>');
-					$("#link").append('<li> <a href = "' + mystream +'" target = "_blank">Offline</a></li>');
-					$('#status').append('<li> Not currently streaming </li>');
+					$('#result').append('<div class="media" ><div class="media-left"><img class = "media-object" src = "'+mylogo+'" /></div><div class="media-body"><h4 class="media-heading">'+myname + '</h4><a href = "' + mystream +'" target = "_blank">Offline</a><br>Not currently streaming </div></div>');
 				}
 				else{ //online
-					$('#displayName').append('<li> '+myname + '</li>');
-					$("#logo").append('<li><img src = "'+mylogo+'" /></li>');
-					$("#link").append('<li> <a href = "' + mystream +'" target = "_blank">Online</a></li>');
-					$('#status').append('<li>'+ data2["stream"]["game"] + ' </li>');
+					$('#result').append('<div class="media" ><div class="media-left"><img class = "media-object" src = "'+mylogo+'" /></div><div class="media-body"><h4 class="media-heading">'+myname + '</h4><a href = "' + mystream +'" target = "_blank">Online</a><br>'+ data2["stream"]["game"] + '<br>'+ data2["stream"]["channel"]["status"] +'</div></div>');
 				}
 			});
-			}
-			else{
-				
-			}
-			
-		},
-		error: function(err){
-			
-				$('#displayName').append('<li> '+streamers[i]+' is invalid</li>');
+    }
+    if(dataI.error){ //not a user
+            $('#result').append('<div class="media"><div class="media-right media-body">'+dataI.message+'</div></div>');
+      }
 			
 		}
 	});
-    /* $.getJSON(url,function(data1){
-    if(data1.stream === null){
-      $("#displayName").prepend(streamers[i]);
-      var link = "https://www.twitch.tv/"+streamers[i];
-      $("#link").attr('href',link);
-       $("#onoff").prepend("Offline ");
-    }
-      else{
-         var logo = data1["stream"]["channel"]["logo"];
-        $("#logo").attr('src',logo);
-        $("#displayName").prepend(data1["stream"]["channel"]["display_name"]);
-        var link = "https://www.twitch.tv/"+streamers[i];
-         $("#link").attr('href',data1["stream"]["channel"]["url"]);
-       $("#onoff").prepend("Online ");
-        $("#status").prepend(data1["stream"]["channel"]["status"]);
-      } */
-     
-     // $("#logo").prepend('<img src="'+logo+'" />');
-   } 
+    } 
    
    
 });
@@ -69,122 +38,3 @@ $(document).ready(function(){
 
 
 
-/* // api call - https://wind-bow.gomix.me/twitch-api
-var streams = ["ESL_SC2", "OgamingSC2", "FreeCodeCamp", "noobs2ninjas","brunofin"];
-
-var data = [
-  {
-    "stream": {
-      "mature": false,
-      "status": "Greg working on Electron-Vue boilerplate w/ Akira #programming #vuejs #electron",
-      "broadcaster_language": "en",
-      "display_name": "FreeCodeCamp",
-      "game": "Creative",
-      "language": "en",
-      "_id": 79776140,
-      "name": "freecodecamp",
-      "created_at": "2015-01-14T03:36:47Z",
-      "updated_at": "2016-09-17T05:00:52Z",
-      "delay": null,
-      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png",
-      "banner": null,
-      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png",
-      "background": null,
-      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_banner-6f5e3445ff474aec-480.png",
-      "profile_banner_background_color": null,
-      "partner": false,
-      "url": "https://www.twitch.tv/freecodecamp",
-      "views": 161989,
-      "followers": 10048,
-      "_links": {
-        "self": "https://api.twitch.tv/kraken/channels/freecodecamp",
-        "follows": "https://api.twitch.tv/kraken/channels/freecodecamp/follows",
-        "commercial": "https://api.twitch.tv/kraken/channels/freecodecamp/commercial",
-        "stream_key": "https://api.twitch.tv/kraken/channels/freecodecamp/stream_key",
-        "chat": "https://api.twitch.tv/kraken/chat/freecodecamp",
-        "subscriptions": "https://api.twitch.tv/kraken/channels/freecodecamp/subscriptions",
-        "editors": "https://api.twitch.tv/kraken/channels/freecodecamp/editors",
-        "teams": "https://api.twitch.tv/kraken/channels/freecodecamp/teams",
-        "videos": "https://api.twitch.tv/kraken/channels/freecodecamp/videos"
-      }
-    },
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/freecodecamp",
-      "channel": "https://api.twitch.tv/kraken/channels/freecodecamp"
-    }
-  },
-  {
-    "stream": null,
-    "display_name": "OgamingSC2",
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/ogamingsc2",
-      "channel": "https://api.twitch.tv/kraken/channels/ogamingsc2"
-    }
-  },
-  {
-    "stream": {
-      "mature": false,
-      "status": "RERUN: StarCraft 2 - Kane vs. HuK (ZvP) - WCS Season 3 Challenger AM - Match 4",
-      "broadcaster_language": "en",
-      "display_name": "ESL_SC2",
-      "game": "StarCraft II",
-      "language": "en",
-      "_id": 30220059,
-      "name": "esl_sc2",
-      "created_at": "2012-05-02T09:59:20Z",
-      "updated_at": "2016-09-17T06:02:57Z",
-      "delay": null,
-      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_image-d6db9488cec97125-300x300.jpeg",
-      "banner": null,
-      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-channel_offline_image-5a8657f8393c9d85-1920x1080.jpeg",
-      "background": null,
-      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_banner-f8295b33d1846e75-480.jpeg",
-      "profile_banner_background_color": "#050506",
-      "partner": true,
-      "url": "https://www.twitch.tv/esl_sc2",
-      "views": 60843789,
-      "followers": 135275,
-      "_links": {
-        "self": "https://api.twitch.tv/kraken/channels/esl_sc2",
-        "follows": "https://api.twitch.tv/kraken/channels/esl_sc2/follows",
-        "commercial": "https://api.twitch.tv/kraken/channels/esl_sc2/commercial",
-        "stream_key": "https://api.twitch.tv/kraken/channels/esl_sc2/stream_key",
-        "chat": "https://api.twitch.tv/kraken/chat/esl_sc2",
-        "subscriptions": "https://api.twitch.tv/kraken/channels/esl_sc2/subscriptions",
-        "editors": "https://api.twitch.tv/kraken/channels/esl_sc2/editors",
-        "teams": "https://api.twitch.tv/kraken/channels/esl_sc2/teams",
-        "videos": "https://api.twitch.tv/kraken/channels/esl_sc2/videos"
-      }
-    },
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
-      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
-    }
-  },
-  {
-    "stream": null,
-    "display_name": "noobs2ninjas",
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
-      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
-    }
-  },
-  {
-    "error": "Not Found",
-    "status": 404,
-    "message": "Channel 'not-a-valid-account' does not exist"
-  }
-];
-
-$(document).ready(function(){
-   for(var i =0; i<streams.length; i++){
-           var result = data.filter(function( obj ) {
-             if((obj.stream !== null)||(obj.stream !== undefined)){
-                  return obj["stream"]["name"] == streams[i];
-             }
-});
-              
-       
-    
-   }
-}); */
