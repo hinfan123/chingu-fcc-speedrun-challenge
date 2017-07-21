6b27bc48a1395493b635f6dc405faf575a523540
Pankajashree <pankajashree@gmail.com>
2017-07-21 23:45:14 +0530

    addd v-model

--- data-vis/markdown-previewer/md-viewer/src/App.vue
+++ data-vis/markdown-previewer/md-viewer/src/App.vue
@@ -3,19 +3,23 @@
     <h1 class="page-header">Markdown Previewer</h1>
     <div id="md" class="col-md-6">
     <h2>Enter Markdown Text</h2>
+    <textarea class='form-control' v-model='md_text'></textarea>
     </div>
     <div id="preview" class="col-md-6">
     <h2>Preview</h2>
+    {{md_preview}}
     </div>
   </div>
 </template>
 
 <script>
+var marked = require('marked');
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      md_preview: md_text
     }
   }
 }
