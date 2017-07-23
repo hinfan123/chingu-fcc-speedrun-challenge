39f3c51224651fe62258dd19182e5229b06ecf5e
Pankajashree <pankajashree@gmail.com>
2017-07-23 10:11:43 +0530

    Add header and footer templates

---
+++ data-vis/markdown-previewer/md-viewer/src/components/header.vue
@@ -0,0 +1,8 @@
+<template>
+<header class="page-header">
+    <h1><a href = "https:\\freecodecamp.com">FreeCodeCamp</a> Data Visualization Project 1 - {{projectname}}</h1>
+</header>
+</template>
+<script>
+import 
+</script>
