a2497e590138ac16a382d056b7aa97abdfacf4e1
Pankajashree <pankajashree@gmail.com>
2017-08-27 14:02:51 +0530

    Add project files

---
+++ data-vis/camper-leaderboard/campers/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  <app-header project-title = 'Data Visualization Project 2: Camper Leaderboard'></app-header>
+    <app-footer></app-footer>
+  </div>
+</template>
+<script>
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+      }
+  },
+  computed: {
+
+  },
+  components: {
+    'app-header': Header,
+    'app-footer': Footer
+  }
+}
+</script>
+
+
+
+
---
+++ data-vis/camper-leaderboard/campers/src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+<header class="page-header">
+    <h1><a href = "https:\\freecodecamp.com">FreeCodeCamp</a> {{projectTitle}}</h1>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+    }
+  },
+  props: ['projectTitle']
+}
+</script>
+
+<style scoped>
+header{
+    backgound-color: green;
+    color: white;
+    text-align: center;
+}
+</style>
+
