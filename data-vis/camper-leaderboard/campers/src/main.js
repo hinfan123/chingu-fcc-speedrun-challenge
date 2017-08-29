ee01fbac69c87035e6740e461c2df42672d0a27a
Pankajashree <pankajashree@gmail.com>
2017-08-29 21:51:31 +0530

    remove material custom theme

--- data-vis/camper-leaderboard/campers/src/main.js
+++ data-vis/camper-leaderboard/campers/src/main.js
@@ -4,13 +4,6 @@ var VueMaterial = require('vue-material')
 
 Vue.use(VueMaterial);
 
-Vue.material.registerTheme('default', {
-  primary: 'teal',
-  accent: 'green',
-  warn: 'red',
-  background: 'grey'
-})
-
 new Vue({
   el: '#app',
   render: h => h(App)
