9088212150daebece06a73850eb0a191dade5e49
Pankajashree <pankajashree@gmail.com>
2017-08-29 21:34:15 +0530

    set material theme colour

--- data-vis/camper-leaderboard/campers/src/main.js
+++ data-vis/camper-leaderboard/campers/src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+var VueMaterial = require('vue-material')
+
+Vue.use(VueMaterial);
+
+Vue.material.registerTheme('default', {
+  primary: 'teal',
+  accent: 'green',
+  warn: 'red',
+  background: 'grey'
+})
 
 new Vue({
   el: '#app',
