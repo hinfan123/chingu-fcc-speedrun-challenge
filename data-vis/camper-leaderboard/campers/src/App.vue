c256834da597438c4965bc8e8bdcdfe2b208b907
Pankajashree <pankajashree@gmail.com>
2017-08-30 21:41:19 +0530

    add content for tab1

--- data-vis/camper-leaderboard/campers/src/App.vue
+++ data-vis/camper-leaderboard/campers/src/App.vue
@@ -6,8 +6,7 @@
             active-tab-color="#9b59b6" 
             active-text-color="white">
   <v-tab title="Past 30 Days Leaders">
-  <table-grid :col-titles = 'colTitles' :col-data = 'colData'>
-  </table-grid>
+  <past></past>
   </v-tab>
   <v-tab title = "All time Leaders">
   <alltime></alltime>
@@ -20,34 +19,22 @@
 import {VueTabs, VTab} from 'vue-nav-tabs'
 import Header from './components/header.vue'
 import Footer from './components/footer.vue'
-import Table from './components/table.vue'
+import past30 from './components/past.vue'
 import allTime from './components/alltime.vue'
 
 export default {
   name: 'app',
   data () {
     return {
-       colTitles: ['#', 'camper Name', 'points in past 30 days', 'all time points'],
-       colData: []
       }
   },
-  methods: {
-     fetchColData: function() {
-       fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-            .then(response => response.json())
-            .then(json => this.colData = json)
-            console.log(this.colData);
-            }
-  },
-  created() {
-    this.fetchColData()
-  },
+  
   components: {
     VueTabs,
     VTab,
     'app-header': Header,
     'app-footer': Footer,
-    'table-grid': Table,
+    'past': past30,
     'alltime': allTime
   }
 }
