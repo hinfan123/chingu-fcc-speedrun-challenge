8ea97b2efc3531d9572508d1cebe0f94a21f8e0a
Pankajashree <pankajashree@gmail.com>
2017-08-30 21:40:54 +0530

    create a component for past30 days leaders

---
+++ data-vis/camper-leaderboard/campers/src/components/past.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="past">
+  <table-grid :col-titles = 'colTitles' :col-data = 'colData'>
+  </table-grid>
+  </div>
+</template>
+
+<script>
+import Table from './table.vue'
+
+export default {
+  name: 'past',
+  data () {
+    return {
+       colTitles: ['#', 'camper Name', 'points in past 30 days', 'all time points'],
+       colData: []
+      }
+  },
+  methods: {
+     fetchColData: function() {
+       fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+            .then(response => response.json())
+            .then(json => this.colData = json)
+            console.log(this.colData);
+            }
+  },
+  created() {
+    this.fetchColData()
+  },
+  components: {
+    'table-grid': Table
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+
+
+
