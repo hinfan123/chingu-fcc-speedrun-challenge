97324f457fba9be5076b49a218c0240fee12dd45
Pankajashree <pankajashree@gmail.com>
2017-09-27 22:10:10 +0530

    create list item view for ingredients

--- data-vis/recipe-box/src/components/recipe.vue
+++ data-vis/recipe-box/src/components/recipe.vue
@@ -8,10 +8,13 @@
     <md-list-item v-for="item in recipes" :key="item.id">
         <span>{{item.title}}</span>
         <md-list-expand>
-        <md-list>
-            <md-list-item class="md-inset">World</md-list-item>
-            <md-list-item class="md-inset">Americas</md-list-item>
-            <md-list-item class="md-inset">Europe</md-list-item>
+          <md-list-item>
+            <span class="md-subheading"><b>Ingredients:</b></span>
+            <md-button>Edit<md-icon>edit</md-icon></md-button>
+            <md-button @click="delItem(index)">Delete<md-icon>delete</md-icon></md-button>
+          </md-list-item>
+        <md-list v-for="item_ing in item.ingredients" :key="item_ing">
+            <md-list-item class="md-inset">{{item_ing}}</md-list-item>
         </md-list>
         </md-list-expand>
     </md-list-item>
