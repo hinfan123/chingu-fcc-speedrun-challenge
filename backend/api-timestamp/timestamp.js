146c67e9279ca806aec9f6802b402db92a3682f6
Pankajashree <pankajashree@gmail.com>
2017-11-07 22:39:31 +0530

    Create timestamp.js

---
+++ backend/api-timestamp/timestamp.js
@@ -0,0 +1,20 @@
+'use strict'
+module.exports.convert = (timestamp) => {
+  if(Number(timestamp)){ //if unix timestamp is given, i.e., a number
+    let d = new Date(timestamp * 1000);
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    let day = d.getDate();
+    return {
+      "unix": timestamp,
+      "natural": month + ' ' + day + ', ' + year
+    }
+  }
+  else {
+    return {
+      "unix": Date.parse(timestamp) / 1000, //convert unix timestamp to string
+      "natural": timestamp
+    }
+  }
+};
